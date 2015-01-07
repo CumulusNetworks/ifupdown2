a57fc3a0bbf69ce1e8ae44df65097f27052deab6
Balakrishnan Raman <ramanb@cumulusnetworks.com>
2015-01-06 22:12:08 -0800

    change ifupdown example for vxlan-learning to the non-default option
    Ticket: CM-4723
    Reviewed By: CCR-2408
    Testing Done:
    
    Change example to non-default option - "vxlan-learning off"

--- addons/vxlan.py
+++ addons/vxlan.py
@@ -25,7 +25,7 @@ class vxlan(moduleBase):
                              'example': ['vxlan-peernodeip 172.16.22.127']},
                         'vxlan-learning' :
                             {'help' : 'vxlan learning on/off',
-                             'example': ['vxlan-learning on'],
+                             'example': ['vxlan-learning off'],
                              'default': 'on'},
                 }}
 
