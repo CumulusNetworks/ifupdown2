0edaee373fee99a784a3daf5ac86f2258c1691ab
Roopa Prabhu <roopa@cumulusnetworks.com>
2015-03-10 14:18:30 -0700

    merge docs.addons content into docs directory
    Ticket:
    Reviewed By:
    Testing Done:

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,10 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../../ifupdown'))
+sys.path.append(os.path.abspath('../../addons'))
+sys.path.append(os.path.abspath('../../'))
+sys.path.append(os.path.abspath('../../ifupdownaddons'))
+sys.path.append(os.path.abspath('../../../ifupdown2'))
 
 # -- General configuration -----------------------------------------------------
 
