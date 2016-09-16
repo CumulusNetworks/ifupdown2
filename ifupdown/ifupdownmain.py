CumulusNetworks/ifupdown2

commit 61c4d7244735cb490229d65a70ab53264d9346d9

Suppress 'Network down' warnings when link_master_slave feature is on
Ticket: CM-4462
Reviewed By:
Testing Done: Tested with interfaces file given in the bug.

The 'network down' msg from the kernel is when the lower device is not
'admin up'. In CM-4462 it is seen when the vlan interface on the bond
is 'admin up' when the bond is still in 'admin down' state.
The bond is also a bridge port so, bond will be 'admin up' when
the bridge it belongs to is brought up.

As link_master_slave feature is on only when all network interfaces
are brought up/down, the states of all interfaces will eventually
converge to 'admin up', so ignoring such transient 'network down' messages.

ifupdown/ifupdownmain.py
@@ -236,6 +236,21 @@ class ifupdownMain(ifupdownBase):
         else:
             self._link_master_slave = False
 
+    def link_master_slave_ignore_error(self, errorstr):
+        # If link master slave flag is set, 
+        # there may be cases where the lowerdev may not be
+        # up resulting in 'Network is down' error
+        # This can happen if the lowerdev is a LINK_SLAVE
+        # of another interface which is not up yet
+        # example of such a case:
+        #   bringing up a vlan on a bond interface and the bond
+        #   is a LINK_SLAVE of a bridge (in other words the bond is
+        #   part of a bridge) which is not up yet
+        if self._link_master_slave:
+           if 'Network is down':
+              return True
+        return False
+
     def get_ifaceobjs(self, ifacename):
         return self.ifaceobjdict.get(ifacename)
 

ifupdown/scheduler.py
@@ -136,8 +136,9 @@ class ifaceScheduler():
                 try:
                     handler(ifupdownobj, ifaceobjs[0])
                 except Exception, e:
-                    ifupdownobj.logger.warn('%s: %s'
-                                            %(ifaceobjs[0].name, str(e)))
+                    if not ifupdownobj.link_master_slave_ignore_error(str(e)):
+                       ifupdownobj.logger.warn('%s: %s'
+                                   %(ifaceobjs[0].name, str(e)))
                     pass
             for ifaceobj in ifaceobjs:
                 cls.run_iface_op(ifupdownobj, ifaceobj, op,

ifupdownaddons/iproute2.py
@@ -368,7 +368,7 @@ class iproute2(utilsBase):
     def link_set_hwaddress(self, ifacename, hwaddress, force=False):
         if not force:
             if self._cache_check('link', [ifacename, 'hwaddress'], hwaddress):
-                return
+               return
         self.link_down(ifacename)
         cmd = 'link set dev %s address %s' %(ifacename, hwaddress)
         if self.ipbatch:

commit 1a6bda12ed0f94bb69ccce4b2ec35c411e2004ce

ifupdownmain: dont catch link_up and link_down errors in ifupdownmain
Ticket: CM-12843
Reviewed By: julien, nikhil
Testing Done: Tested the failing test case in the bug

The scheduler already has some checking for errors from
link_up and link_down introduced by
commit 61c4d7244735 ("Suppress 'Network down' warnings when
link_master_slave feature is on").

The above fix was removed by a recent commit
707aeb737882 ("netlink ip link set up/down may silently fail, adding
try/except statements"). The scheduler does catch these errors.
so we will need to figure out why the scheduler is not catching it.
Right now this is failing an automation test. So the best fix
is to make sure the previous behaviour is restored.

Signed-off-by: Roopa Prabhu <roopa@cumulusnetworks.com>

ifupdown/ifupdownmain.py
@@ -129,11 +129,7 @@ class ifupdownMain(ifupdownBase):
             return
         if not self.link_exists(ifaceobj.name):
            return
-        try:
-            self.link_up(ifaceobj.name)
-        except Exception as e:
-            self.logger.error('%s: %s' % (ifaceobj.name, str(e)))
-            ifaceobj.set_status(ifaceStatus.ERROR)
+        self.link_up(ifaceobj.name)
 
     def run_down(self, ifaceobj):
         if ((ifaceobj.link_kind & ifaceLinkKind.VRF) or
@@ -157,11 +153,7 @@ class ifupdownMain(ifupdownBase):
            return
         if not self.link_exists(ifaceobj.name):
            return
-        try:
-            self.link_down(ifaceobj.name)
-        except Exception as e:
-            self.logger.error('%s: %s' % (ifaceobj.name, str(e)))
-            ifaceobj.set_status(ifaceStatus.ERROR)
+        self.link_down(ifaceobj.name)
 
     # ifupdown object interface operation handlers
     ops_handlers = OrderedDict([('up', run_up),