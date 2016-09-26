378c6c2681bd3ac6eda188cf9beeb7fd5ae91224
Julien Fortin <julien@cumulusnetworks.com>
2016-09-26 10:37:33 +0200

    ifupdownmain: sending ifaceobj to syntax_check method
    Ticket: None
    Reviewed By: Roopa, Nikhil G
    
    Signed-off-by: Julien Fortin <julien@cumulusnetworks.com>

--- ifupdown/ifupdownmain.py
+++ ifupdown/ifupdownmain.py
@@ -1088,7 +1088,7 @@ class ifupdownMain(ifupdownBase):
                                                      module._modinfo.get('attrs', {})):
                             result = False
                     if hasattr(module, 'syntax_check') and callable(module.syntax_check):
-                        if not module.syntax_check(self.get_ifaceobjs(ifacename)):
+                        if not module.syntax_check(self.get_ifaceobjs(ifacename)[0]):
                             result = False
                 except Exception, e:
                     self.logger.warn('%s: %s' % (ifacename, str(e)))
