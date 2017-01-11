2775a2db5fef4e7cf9e70dd0469a24251c9c27a4
Julien Fortin <julien@cumulusnetworks.com>
2017-01-12 02:48:00 +0300

    ifupdown: exceptions: add ArgvParseError
    Signed-off-by: Julien Fortin <julien@cumulusnetworks.com>

--- ifupdown/exceptions.py
+++ ifupdown/exceptions.py
@@ -7,10 +7,31 @@
 #    exceptions
 #
 
+from ifupdown.log import log
+
+
 class Error(Exception):
     """Base class for exceptions in ifupdown"""
 
+    def log_error(self):
+        log.error(self.message)
+
+    def log_warning(self):
+        log.warning(self.message)
+
+    def log_info(self):
+        log.info(self.message)
+
+    def log_debug(self):
+        log.debug(self.message)
+
+
+class ArgvParseError(Error):
+    """
+        Exception coming from argv parsing
+    """
     pass
+
 
 class ifaceNotFoundError(Error):
     pass
@@ -19,8 +40,10 @@ class ifaceNotFoundError(Error):
 class invalidValueError(Error):
     pass
 
+
 class errorReadingStateError(Error):
     pass
+
 
 class moduleNotSupported(Error):
     pass
