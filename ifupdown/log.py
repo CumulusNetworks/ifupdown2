a8b5d2c5f4a4258e1b4d71f027c4f97f7cb03a15
Julien Fortin <julien@cumulusnetworks.com>
2017-02-07 05:31:27 +0300

    ifupdown2d: log: re-write Log class, better handle log syslog/console
    Signed-off-by: Julien Fortin <julien@cumulusnetworks.com>

--- ifupdown/log.py
+++ ifupdown/log.py
@@ -6,6 +6,7 @@
 #
 
 import sys
+import json
 import logging
 import logging.handlers
 
@@ -26,126 +27,156 @@ class Log:
                 client.
                 if -l or --syslog we make sure to use syslog.
         """
-        self.logger = None
 
         self.stdout_buffer = None
         self.stderr_buffer = None
 
-        self.root_logger = logging.getLogger()
-        self.log_level = Log.get_log_level(args=None, verbose=True)
+        self.root = logging.getLogger()
+        self.root.name = Log.LOGGER_NAME
+        self.root.setLevel(logging.INFO)
 
-        facility = logging.handlers.SysLogHandler.LOG_DAEMON
-        self.syslog_handler = logging.handlers.SysLogHandler(address='/dev/log',
-                                                             facility=facility)
+        self.root_info = self.root.info
+        self.root_debug = self.root.debug
+        self.root_error = self.root.error
+        self.root_warning = self.root.warning
+        self.root_critical = self.root.critical
 
-        logging.addLevelName(logging.ERROR, 'error')
+        self.root.info = self.info
+        self.root.debug = self.debug
+        self.root.error = self.error
+        self.root.warning = self.warning
+        self.root.critical = self.critical
+
+        logging.addLevelName(logging.CRITICAL, 'critical')
         logging.addLevelName(logging.WARNING, 'warning')
+        logging.addLevelName(logging.ERROR, 'error')
         logging.addLevelName(logging.DEBUG, 'debug')
         logging.addLevelName(logging.INFO, 'info')
 
-        format = '%(name)s: %(levelname)s: %(message)s'
-        self.syslog_handler.setFormatter(logging.Formatter(format))
-        self.root_logger.addHandler(self.syslog_handler)
-        self.root_logger.setLevel(self.log_level)
+        self.syslog = True
+        self.socket = None
 
-        self.logger = logging.getLogger(Log.LOGGER_NAME)
-        self.logging_syslog = True
+        # syslog
+        facility = logging.handlers.SysLogHandler.LOG_DAEMON
+        address = '/dev/log'
+        format = '%(name)s: %(levelname)s: %(message)s'
+
+        self.syslog_handler = logging.handlers.SysLogHandler(address=address, facility=facility)
+        self.syslog_handler.setFormatter(logging.Formatter(format))
+
+        # console
+        format = '%(levelname)s: %(message)s'
+        self.console_handler = logging.StreamHandler()
+        self.console_handler.setFormatter(logging.Formatter(format))
+
+        self.update_current_logger(syslog=self.syslog, verbose=True, debug=False)
+
+    def update_current_logger(self, syslog, verbose, debug):
+        self.syslog = syslog
+        self.root.setLevel(self.get_log_level(verbose=verbose, debug=debug))
+        self.root.handlers = [self.syslog_handler if self.syslog else self.console_handler]
+        self.flush()
+
+    def flush(self):
+        if self.socket:
+            result = dict()
+            stdout = self._flush_buffer('stdout', self.stdout_buffer, result)
+            stderr = self._flush_buffer('stderr', self.stderr_buffer, result)
+
+            if stdout or stderr:
+                self.socket.sendall(json.dumps(result))
+                self.redirect_stdouput()
+
+    def set_socket(self, socket):
+        self.socket = socket
+        self.redirect_stdouput()
+
+    def redirect_stdouput(self):
+        self.stdout_buffer = sys.stdout = StringIO()
+        self.stderr_buffer = self.console_handler.stream = sys.stderr = StringIO()
+
+    def error(self, msg, *args, **kwargs):
+        self.root_error(msg, *args, **kwargs)
+        self.flush()
+
+    def critical(self, msg, *args, **kwargs):
+        self.root_critical(msg, *args, **kwargs)
+        self.flush()
+
+    def warning(self, msg, *args, **kwargs):
+        self.root_warning(msg, *args, **kwargs)
+        self.flush()
+
+    def info(self, msg, *args, **kwargs):
+        self.root_info(msg, *args, **kwargs)
+        self.flush()
+
+    def debug(self, msg, *args, **kwargs):
+        self.root_debug(msg, *args, **kwargs)
+        self.flush()
+
+    def get_current_log_level(self):
+        return self.root.level
+
+    def is_syslog(self): return self.syslog
 
     @staticmethod
-    def get_log_level(args, verbose):
-        if not args and verbose:
-            return logging.INFO
+    def get_log_level(verbose=False, debug=False):
         log_level = logging.WARNING
-        if not args:
-            return log_level
-        if args.debug:
+        if debug:
             log_level = logging.DEBUG
-        elif args.verbose:
+        elif verbose:
             log_level = logging.INFO
         return log_level
 
     @staticmethod
-    def request_syslog(args, syslog):
-        return args.syslog if args and hasattr(args, 'syslog') else syslog
-
-    def update_logger(self, args=None, verbose=True, syslog=False):
-        """
-            Check if we need to update the current logger+level (syslog or std)
-        """
-        self.log_level = Log.get_log_level(args=args, verbose=verbose)
-        request_syslog = self.request_syslog(args=args, syslog=syslog)
-
-        if self.logging_syslog and not request_syslog:
-            # set regular logger
-            self.logging_syslog = False
-            self.flush()
-
-        elif not self.logging_syslog and request_syslog:
-            # set syslog logger
-            self.root_logger.addHandler(self.syslog_handler)
-            self.logging_syslog = True
-
-            self.stdout_buffer = None
-            self.stderr_buffer = None
-
-            sys.stdout = sys.__stdout__
-            sys.stderr = sys.__stderr__
-
-        elif self.logging_syslog:
-            self.root_logger.setLevel(self.log_level)
-
-        self.logger.setLevel(self.log_level)
-
-    def set_basic_config(self):
-        logging.basicConfig(level=self.log_level,
-                            format='%(levelname)s: %(message)s')
-
-    def flush(self):
-        """
-            We need to flush the buffers between each command execution:
-
-            logging is using stderr, we previously overrided sys.stderr with a
-            StringIO buffer to be able to redirect the output of the process to
-            the UNIX socket.
-            For some reasons logging is not using sys.stderr direct, it must be
-            storing a copy of the object in sys.stderr because simply resetting
-            the buffer in sys.stderr doesn't work.
-
-            The workaround is to remove all existing handlers and restart from
-            scratch with:
-                logging.basicConfig
-
-        """
-        [logging.root.removeHandler(handler) for handler in logging.root.handlers[:]]
-        [self.logger.removeHandler(handler) for handler in self.logger.handlers[:]]
-
-        self.stderr_buffer = sys.stderr = StringIO()
-        self.stdout_buffer = sys.stdout = StringIO()
-        self.set_basic_config()
-
-        if self.logging_syslog:
-            self.root_logger.addHandler(self.syslog_handler)
-
-    def info(self, str):
-        self.logger.info(str)
-
-    def debug(self, str):
-        self.logger.debug(str)
-
-    def warning(self, str):
-        self.logger.warning(str)
-
-    def error(self, str):
-        self.logger.error(str)
-
-    def get_stdout_buffer(self):
-        return self.stdout_buffer.getvalue() if self.stdout_buffer else ''
-
-    def get_stderr_buffer(self):
-        return self.stderr_buffer.getvalue() if self.stderr_buffer else ''
-
-    def syslog_mode(self):
-        return self.logging_syslog
+    def _flush_buffer(stream, buff, dictionary):
+        if buff:
+            data = buff.getvalue()
+            if data:
+                dictionary[stream] = data
+                return True
 
 
 log = Log()
+
+
+class Logger(logging.Logger):
+    def __init__(self, name, level=log.get_current_log_level()):
+        logging.Logger.__init__(self, name, level)
+        log.debug('%s: init logger level %s' % (name, level))
+
+    def debug(self, msg, *args, **kwargs):
+        log.debug(msg, *args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        log.info(msg, *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        log.warning(msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        log.error(msg, *args, **kwargs)
+
+
+logging.setLoggerClass(Logger)
+
+
+"""
+
+#logging.basicConfig( format="%(filename)s: %(username)s says '%(message)s' in %(funcname)s" )
+
+Logger.debug(msg, *args, **kwargs)
+Logs a message with level DEBUG on this logger. The msg is the message format string, and the args are the arguments which are merged into msg using the string formatting operator. (Note that this means that you can use keywords in the format string, together with a single dictionary argument.)
+
+There are two keyword arguments in kwargs which are inspected: exc_info which, if it does not evaluate as false, causes exception information to be added to the logging message. If an exception tuple (in the format returned by sys.exc_info()) is provided, it is used; otherwise, sys.exc_info() is called to get the exception information.
+
+"""
+
+"""
+USE FILTER TO IGNORE "EXITS" MESSAGES
+Now that you know the basic plot, let me introduce one more character - the Filter.
+Filter as the name suggests, allows you to filter a message before you log it. Yes, messages are filtered based on the level setting, but adding a Filter gives you more fine grained control of messages you log.
+Both Loggers and Handlers can have multiple Filters. You can add Filters using addFilter and removeFilter methods.
+When a Logger/Handler receives a message, it consults all of its filters. If the filter(record) method on any of the Filters attached returns False (or 0) the message is dropped.
+The official documentation, though detailed, is actually pretty confusing about the role of Filters. This is a pity; because Filters can be handy when you want to drop a message based on a regular expression, error code, contextual information and pretty much anything else. The default Filter is pretty much useless (and the doc string is very confusing too). Just inherit from the default filter and override the filter method according to what you want to filter out. (Be sure to download the source for logging module and check out the unit tests which have some good examples. See the references at the end of this post.)"""
