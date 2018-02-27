CumulusNetworks/ifupdown2

commit 63039f3f5e03f9caa43c6f9b71a8318afea93289

netlink: if netlink call fail, fall back on iproute2/sysfs backend
Ticket: CM-11594
Reviewed By: Roopa, Nikhil G
Testing Done: ifupdown2-tests, smoke/nightly ifupdown2 + ubuntu test

Signed-off-by: Julien Fortin <julien@cumulusnetworks.com>

addons/vxlan.py
@@ -185,11 +185,25 @@ class vxlan(moduleBase):
                     ageing = self.get_attr_default_value('vxlan-ageing')
 
             if self.should_create_set_vxlan(link_exists, ifname, vxlanid, local, learning, ageing, group):
-                netlink.link_add_vxlan(ifname, vxlanid,
-                                       local=local,
-                                       learning=learning,
-                                       ageing=ageing,
-                                       group=group)
+                try:
+                    netlink.link_add_vxlan(ifname, vxlanid,
+                                           local=local,
+                                           learning=learning,
+                                           ageing=ageing,
+                                           group=group)
+                except Exception as e_netlink:
+                    self.logger.debug('%s: vxlan netlink: %s' % (ifname, str(e_netlink)))
+                    try:
+                        self.ipcmd.link_create_vxlan(ifname, vxlanid,
+                                                     localtunnelip=local,
+                                                     svcnodeip=group,
+                                                     remoteips=ifaceobj.get_attr_value('vxlan-remoteip'),
+                                                     learning='on' if learning else 'off',
+                                                     ageing=anycastip)
+                    except Exception as e_iproute2:
+                        self.logger.warning('%s: vxlan add/set failed: %s' % (ifname, str(e_iproute2)))
+                        return
+
                 try:
                     # manually adding an entry to the caching after creating/updating the vxlan
                     if not ifname in linkCache.links:

addons/addressvirtual.py
@@ -188,7 +188,10 @@ class addressvirtual(moduleBase):
             link_created = False
             macvlan_ifacename = '%s%d' %(macvlan_prefix, av_idx)
             if not self.ipcmd.link_exists(macvlan_ifacename):
-                netlink.link_add_macvlan(ifaceobj.name, macvlan_ifacename)
+                try:
+                    netlink.link_add_macvlan(ifaceobj.name, macvlan_ifacename)
+                except:
+                    self.ipcmd.link_add_macvlan(ifaceobj.name, macvlan_ifacename)
                 link_created = True
             ips = av_attrs[1:]
             if mac != 'None':

ifupdownaddons/LinkUtils.py
@@ -139,6 +139,15 @@ class LinkUtils(utilsBase):
                 if not ifla_master:
                     raise Exception('No master associated with bridge port %s' % ifname)
 
+                for nl_attr in [
+                    Link.IFLA_BRPORT_STATE,
+                    Link.IFLA_BRPORT_COST,
+                    Link.IFLA_BRPORT_PRIORITY,
+                ]:
+                    if nl_attr not in info_slave_data and LinkUtils.bridge_utils_is_installed:
+                        self._fill_bridge_info_brctl()
+                        return
+
                 brport_attrs = {
                     'pathcost': str(info_slave_data.get(Link.IFLA_BRPORT_COST, 0)),
                     'fdelay': format(float(info_slave_data.get(Link.IFLA_BRPORT_FORWARD_DELAY_TIMER, 0) / 100), '.2f'),
@@ -332,15 +341,21 @@ class LinkUtils(utilsBase):
                 linkCache.set_attr([bondname, 'linkinfo', 'slaves'],
                                    self.read_file_oneline('/sys/class/net/%s/bonding/slaves'
                                                           % bondname).split())
+                try:
+                    # if some attribute are missing we try to get the bond attributes via sysfs
+                    bond_linkinfo = linkCache.links[bondname]['linkinfo']
+                    for attr in ['mode', 'xmit_hash_policy', 'min_links']:
+                        if attr not in bond_linkinfo:
+                            self._fill_bond_info_sysfs(bondname)
+                            # after we fill in the cache we can continue to the next bond
+                            break
+                except:
+                    self._fill_bond_info_sysfs(bondname)
+
             except Exception as e:
                 self.logger.debug('LinkUtils: bond cache error: %s' % str(e))
 
-                # self._fill_bond_info_sysfs()
-
     def _fill_bond_info_sysfs(self, bondname):
-        return
-        # move bondutils backend to use netlink for now.
-        # we are keeping the code for a future use
         try:
             linkCache.set_attr([bondname, 'linkinfo', 'min_links'],
                                self.read_file_oneline(
@@ -1083,7 +1098,7 @@ class LinkUtils(utilsBase):
             utils.exec_command('%s %s' % (utils.ip_cmd, cmd))
 
         # XXX: update linkinfo correctly
-        self._cache_update([name], {})
+        #self._cache_update([name], {})
 
     @staticmethod
     def link_exists(ifacename):
@@ -1100,6 +1115,10 @@ class LinkUtils(utilsBase):
         if re.search(r'\.', ifacename):
             return True
         return False
+
+    @staticmethod
+    def link_add_macvlan(ifname, macvlan_ifacename):
+        utils.exec_commandl(['ip', 'link', 'add',  'link', ifname, 'name', macvlan_ifacename, 'type', 'macvlan', 'mode', 'private'])
 
     @staticmethod
     def route_add(route):

commit 34fba123a2f02dca95c51a9390c25eb3888ba536

addons: vxlan: fix ageing argument value on ip link call (completion typo)
Ticket: CM-19882
Reviewed By: Roopa, Nikhil G

regression introduced in commit 63039f3f5e03f9caa43c6f9b71a8318afea93289
most likely due to a auto-completion error: type "a" then enter on the first
suggestion of the autocompletion list anycastip instead of ageing variable

Signed-off-by: Julien Fortin <julien@cumulusnetworks.com>

addons/vxlan.py
@@ -238,7 +238,7 @@ class vxlan(moduleBase):
                                                      svcnodeip=group,
                                                      remoteips=ifaceobj.get_attr_value('vxlan-remoteip'),
                                                      learning='on' if learning else 'off',
-                                                     ageing=anycastip)
+                                                     ageing=ageing)
                     except Exception as e_iproute2:
                         self.logger.warning('%s: vxlan add/set failed: %s' % (ifname, str(e_iproute2)))
                         return