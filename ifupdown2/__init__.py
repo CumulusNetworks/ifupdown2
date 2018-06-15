db61a1a8a01a750775a5c3506d02c13633461f3d
Julien Fortin <julien@cumulusnetworks.com>
2018-06-15 17:58:26 +0200

    debian: changelog: change version to 1.2.0
    Signed-off-by: Julien Fortin <julien@cumulusnetworks.com>

--- ifupdown2/__init__.py
+++ ifupdown2/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = '2.0.0'
+__version__ = '1.2.0'
 
 # Copyright (C) 2014,2015,2016,2017,2018 Cumulus Networks, Inc. All rights reserved
 #
--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     name='ifupdown2',
     packages=find_packages(),
     url='https://github.com/CumulusNetworks/ifupdown2',
-    version='2.0.0',
+    version='1.2.0',
     data_files=DATA_FILES,
     setup_requires=['setuptools'],
     scripts=SCRIPTS,
