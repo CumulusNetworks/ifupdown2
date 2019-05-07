0379aaf3a126946b7fafc6f20261a1151143d77d
Julien Fortin <julien@cumulusnetworks.com>
2019-05-08 00:25:55 +0800

    lib: dry_run: fix DRY-RUN handler using weakrefs
    We disabled DRY-RUN mode because it was introducing huge memory leaks. The
    cause of this issue was that we kept strong references on objects/modules
    and methods in our internal data-structure.
    
    To solve this problem we need to move all the existing dry-run handling code
    to use weakrefs. Then another problem emerged ; you can't use the weakref
    module on bound methods. To solve this last issue we had to get some inspira
    tion from ActiveState recipe 81253, implementing customs class to hold
    weakrefs on bound methods but using a ingenious trick to call the dry-run
    method when needed.
    
    Signed-off-by: Julien Fortin <julien@cumulusnetworks.com>

--- ifupdown2/lib/dry_run.py
+++ ifupdown2/lib/dry_run.py
@@ -1,38 +1,95 @@
+#!/usr/bin/python
+# Copyright (C) 2019 Cumulus Networks, Inc. all rights reserved
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; version 2.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+# https://www.gnu.org/licenses/gpl-2.0-standalone.html
+#
+# Author:
+#       Julien Fortin, julien@cumulusnetworks.com
+#
+# ifupdown2 -- dry_run module
+#
+#
+# __WeakMethodBound and __WeakMethodFree classes as well as the WeakMethod
+# function were inspired by the implementation of ActiveState recipes 81253
+# weakmethod.
+# This code solves an important issue here. You can't have weakrefs on bound
+# methods. Here is quote from the recipie:
+#
+# "Normal weakref.refs to bound methods don't quite work the way one expects,
+# because bound methods are first-class objects; weakrefs to bound methods are
+# dead-on-arrival unless some other strong reference to the same bound method
+# exists."
+#
+
+import logging
 import inspect
+import weakref
+
+
+class __WeakMethodBound:
+    """ ActiveState recipes 81253-weakmethod """
+
+    def __init__(self, f):
+        self.f = f.im_func
+        self.c = weakref.ref(f.im_self)
+
+    def __call__(self, *arg, **kwargs):
+        if not self.c():
+            raise TypeError("Method called on dead object")
+        apply(self.f, (self.c(),) + arg, kwargs)
+
+
+class __WeakMethodFree:
+    """ ActiveState recipes 81253-weakmethod """
+
+    def __init__(self, f):
+        self.f = weakref.ref(f)
+
+    def __call__(self, *arg, **kwargs):
+        if not self.f():
+            raise TypeError("Function no longer exist")
+        apply(self.f(), arg, kwargs)
+
+
+def WeakMethod(f):
+    """ ActiveState recipes 81253-weakmethod """
+    try:
+        f.im_func
+    except AttributeError:
+        return __WeakMethodFree(f)
+    return __WeakMethodBound(f)
+
+
+def _weakref_call_back_delete(reference):
+    try:
+        DryRunManager.get_instance().unregister_dry_run_handler_weakref_callback(reference)
+    except:
+        pass
 
 
 class DryRun(object):
     """
     Detect dry_run functions and save the associated handler
     """
+    __DRY_RUN_PREFIX = "DRY-RUN"
+
     def __init__(self):
-        # disabling DryRun as it's creating a huge memory leak in daemon mode
-        # this needs to be fixed before re-enabling dryruns
-        # context: in daemon mode, each time we create an object with DryRun
-        # inheritance, a reference to this object and its code is added to the
-        # DryRunManager via DryRunEntry and stored in a list
-        # DryRunManager:__entries, this list keeps growing and keeps refs to
-        # objects that should be collected by the garbage collector.
-        # A potential fix would be to have a dictionary to store those entries
-        # instead of a list, something like:
-        # {
-        #   "module": {
-        #       "base_attr_name": {
-        #           base_attr_value: attr_value
-        #       }
-        # }
-        # This needs to be studied a bit longer...
-        #
-        # Also the destructor of each objects should make sure to remove it's
-        # DryRunEntries from the dict when the object is removed?
-        # -> Not sure this would work... Since many objects of the same type
-        # can exists at the same time
-        #
-        # It's probably fine to keep a dictionary (without duplicates)
-        # of all the module and dry_run entries.
-        # Once a new object is created (with DryRun inheritance) we can look
-        # in this "cache" to see if we already have an existing dryrun entry
-        # and re-use this one.
+        self.logger = logging.getLogger("ifupdown2.%s" % self.__class__.__name__)
+
         for attr_name in dir(self):
             try:
                 # We need to iterate through the object attribute
@@ -57,13 +114,19 @@ class DryRun(object):
                     # - the associated dry_run code
                     # we will now register this couple in the DryRunManager
                     DryRunManager.get_instance().register_dry_run_handler(
-                        self,
+                        weakref.ref(self, _weakref_call_back_delete),
                         handler_name=base_attr_name,
-                        handler_code=base_attr_value,
-                        dry_run_code=attr_value
+                        handler_code_weakref=WeakMethod(base_attr_value),
+                        dry_run_code_weakref=WeakMethod(attr_value)
                     )
             except:
                 pass
+
+    def log_info_ifname_dry_run(self, ifname, string):
+        self.logger.info("DRY-RUN: %s: %s" % (ifname, string))
+
+    def log_info_dry_run(self, string):
+        self.logger.info("DRY-RUN: %s" % string)
 
     @staticmethod
     def __is_method_or_function(obj):
@@ -71,20 +134,34 @@ class DryRun(object):
 
 
 class _DryRunEntry(object):
-    def __init__(self, target_module, handler_name, handler_code, dry_run_code):
-        self.target_module = target_module
+    def __init__(self, target_module_weakref, handler_name, handler_code_weakref, dry_run_code_weakref):
+        self.target_module_weakref = target_module_weakref
         self.handler_name = handler_name
-        self.handler_code = handler_code
-        self.dry_run_code = dry_run_code
+        self.handler_code_weakref = handler_code_weakref
+        self.dry_run_code_weakref = dry_run_code_weakref
         self.__status = False
 
     def set(self):
-        self.target_module.__dict__[self.handler_name] = self.dry_run_code
-        self.__status = True
+        target_module_ref = self.target_module_weakref()
+
+        if target_module_ref:
+            if self.dry_run_code_weakref:
+                target_module_ref.__dict__[self.handler_name] = self.dry_run_code_weakref
+                self.__status = True
+        else:
+            # if the reference is dead we need to unregister it
+            DryRunManager.get_instance().unregister_dry_run_handler_weakref_callback(self.target_module_weakref)
 
     def unset(self):
-        self.target_module.__dict__[self.handler_name] = self.handler_code
-        self.__status = False
+        target_module_ref = self.target_module_weakref()
+
+        if target_module_ref:
+            if self.handler_code_weakref:
+                target_module_ref.__dict__[self.handler_name] = self.handler_code_weakref
+                self.__status = False
+        else:
+            # if the reference is dead we need to unregister it
+            DryRunManager.get_instance().unregister_dry_run_handler_weakref_callback(self.target_module_weakref)
 
     def get_status(self):
         return self.__status
@@ -105,27 +182,50 @@ class DryRunManager(object):
         else:
             DryRunManager.__instance = self
 
-        self.__entries = list()
+        self.__entries = dict()
         self.__is_on = False
 
-    def register_dry_run_handler(self, module, handler_name, handler_code, dry_run_code):
+    def register_dry_run_handler(self, module_weakref, handler_name, handler_code_weakref, dry_run_code_weakref):
+        """
+        Register the dry run handler only using weakrefs - we don't want to mess up with garbage collection
+        :param module_weakref:
+        :param handler_name:
+        :param handler_code_weakref:
+        :param dry_run_code_weakref:
+        :return:
+        """
         dry_run_entry = _DryRunEntry(
-            target_module=module,
+            target_module_weakref=module_weakref,
             handler_name=handler_name,
-            handler_code=handler_code,
-            dry_run_code=dry_run_code
+            handler_code_weakref=handler_code_weakref,
+            dry_run_code_weakref=dry_run_code_weakref
         )
         if self.__is_on:
             dry_run_entry.set()
-        self.__entries.append(dry_run_entry)
+
+        if module_weakref in self.__entries:
+            self.__entries[module_weakref].append(dry_run_entry)
+        else:
+            self.__entries[module_weakref] = [dry_run_entry]
+
+    def unregister_dry_run_handler_weakref_callback(self, reference):
+        """
+        If we detect a dead reference, we should remove this reference from our
+        internal data structure
+        """
+        try:
+            del self.__entries[reference]
+        except:
+            pass
 
     def dry_run_mode_on(self):
         """
         Enable the dry run mode
         WARNING: not thread-safe
         """
-        for entry in self.__entries:
-            entry.set()
+        for entries in self.__entries.itervalues():
+            for entry in entries:
+                entry.set()
         self.__is_on = True
 
     def dry_run_mode_off(self):
@@ -133,15 +233,17 @@ class DryRunManager(object):
         Disable the dry run mode
         WARNING: not thread-safe
         """
-        for entry in self.__entries:
-            entry.unset()
+        for entries in self.__entries.itervalues():
+            for entry in entries:
+                entry.unset()
         self.__is_on = False
 
     def dump_entries_stdout(self):
         print "== DryRunManager dump =="
         print "  MODULE:    HANDLER  STATUS"
-        for entry in self.__entries:
-            print "  %s: %s %s" % (repr(entry.target_module), entry.handler_name, "ON" if entry.get_status() else "OFF")
+        for entries in self.__entries.itervalues():
+            for entry in entries:
+                print "  %s: %s() %s" % (repr(entry.target_module_weakref), entry.handler_name, "ON" if entry.get_status() else "OFF")
         print "========================"
 
     def is_dry_mode_on(self):
