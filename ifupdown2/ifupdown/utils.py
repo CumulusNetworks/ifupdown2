6ab519dd598a30f217a14c50cee68e73e5e602c7
Julien Fortin <julien@cumulusnetworks.com>
2019-10-02 18:31:53 +0200

    python3: ch.communicate input should be bytes not str
    Signed-off-by: Julien Fortin <julien@cumulusnetworks.com>

--- ifupdown2/ifupdown/utils.py
+++ ifupdown2/ifupdown/utils.py
@@ -412,7 +412,7 @@ class utils():
                                   stderr=stderr)
             utils.enable_subprocess_signal_forwarding(ch, signal.SIGINT)
             if stdout or stdin:
-                cmd_output = ch.communicate(input=stdin)[0]
+                cmd_output = ch.communicate(input=stdin.encode() if stdin else stdin)[0]
             cmd_returncode = ch.wait()
         except Exception as e:
             raise Exception('cmd \'%s\' failed (%s)' % (' '.join(cmd), str(e)))
