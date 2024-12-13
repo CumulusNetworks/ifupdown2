834bd6db936eb3f55f56a8b565b93b33cda0bae3
Julien Fortin <jfortin@nvidia.com>
2024-12-13 18:31:12 +0100

    debian: changelog: new entry for version 3.9.0
    Signed-off-by: Julien Fortin <jfortin@nvidia.com>

--- ifupdown2/__init__.py
+++ ifupdown2/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version__ = '3.0.0'
+__version__ = '3.9.0'
 
 # Copyright (C) 2014,2015,2016,2017,2018,2019,2020 Cumulus Networks, Inc. All rights reserved
 #
--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     name='ifupdown2',
     packages=find_packages(),
     url='https://github.com/CumulusNetworks/ifupdown2',
-    version='3.0.0',
+    version='3.9.0',
     data_files=DATA_FILES,
     setup_requires=['setuptools'],
     scripts=SCRIPTS,
